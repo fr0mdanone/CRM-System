09c5487db3f93e91b9c3d77210791248a2cc1458
fr0mdanone <167985684+fr0mdanone@users.noreply.github.com>
2025-04-24 11:00:39 +0500

    Revert "[feat] add functionality"

--- src/App.jsx
+++ src/App.jsx
@@ -1,110 +1,35 @@
-import { useState, useEffect } from "react";
-
-import "./App.css";
-import AddUserTask from "./Components/AddUserTask";
-import Tasks from "./Components/Tasks";
-import FilterButtons from "./Components/FilterButtons";
-import EditModal from "./Components/EditModal";
-import ErrorModal from "./Components/ErrorModal";
-
-import {
-	getTasks,
-	deleteTask,
-	addTask,
-	editTask,
-} from "./API/FetchingFunctions";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-	const [error, setError] = useState();
-	const [updatedTasks, setUpdatedTasks] = useState([]);
-	const [filter, setFilter] = useState("all");
-	const [isEditing, setIsEditing] = useState();
+  const [count, setCount] = useState(0)
 
-	function setTasksFilter(id) {
-		setFilter(id);
-	}
-
-	useEffect(() => {
-		async function fetchingTasks() {
-			try {
-				const response = await getTasks(filter);
-				console.log(response);
-
-				setUpdatedTasks(response.data);
-			} catch (error) {
-				setError(error);
-			}
-		}
-		fetchingTasks();
-	}, [filter]);
-
-	async function deletingTask(id) {
-		try {
-			await deleteTask(id);
-			setUpdatedTasks((prevTasksArray) =>
-				prevTasksArray.filter((task) => task.id != id)
-			);
-		} catch (error) {
-			setError(error);
-		}
-	}
-
-	async function addingTask(taskTitle) {
-		try {
-			const userData = {
-				title: taskTitle,
-				isDone: false,
-			};
-
-			const response = await addTask(userData);
-			console.log(response);
-			setUpdatedTasks((prevTasksArray) => [...prevTasksArray, response]);
-		} catch (error) {
-			setError(error);
-		}
-	}
-
-	function editingData(task) {
-		setIsEditing(task);
-	}
-
-	async function submittingChanges(task) {
-		try {
-			await editTask(task);
-			const updatingEditingTasks = updatedTasks.map((oldTask) => {
-				return oldTask.id === task.id ? task : oldTask;
-			});
-			setUpdatedTasks(updatingEditingTasks);
-		} catch (error) {
-			setError(error);
-		}
-	}
-
-	return (
-		<>
-			{isEditing && (
-				<EditModal
-					incomingTask={isEditing}
-					onSaveChanges={submittingChanges}
-					onCancelChanges={() => {
-						setIsEditing(null);
-					}}
-				/>
-			)}
-			{error && (
-				<ErrorModal error={error} onErrorConfirm={() => setError(null)} />
-			)}
-			<AddUserTask onAddTask={addingTask} />
-			<section>
-				<FilterButtons onClick={setTasksFilter} />
-				<Tasks
-					tasks={updatedTasks}
-					onDelete={deletingTask}
-					onEdit={editingData}
-				/>
-			</section>
-		</>
-	);
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
