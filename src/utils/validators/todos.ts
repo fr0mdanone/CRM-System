579e931c098257dd8cce8776dde9df4aa0d37fb9
fr0mdanone <peachtreespw@gmail.com>
2025-09-05 15:54:43 +0500

    [refactor] remove redundant function from utils/todos.ts

--- src/utils/validators/todos.ts
+++ src/utils/validators/todos.ts
@@ -7,17 +7,3 @@ export const validateTodoTitle = (todoTitle: string): boolean => {
 	const todoLength = todoTitle.trim().length;
 	return todoLength >= TODO_TITLE_MIN && todoLength <= TODO_TITLE_MAX;
 };
-
-// export const validateAndHandleTodo = (
-// 	taskTitle: string,
-// 	event: FormEvent<HTMLFormElement>,
-// 	fetchFunc: () => Promise<void>,
-// 	setState: Dispatch<SetStateAction<boolean>>
-// ): void => {
-// 	event.preventDefault();
-// 	if (validateTodoTitle(taskTitle)) {
-// 		fetchFunc();
-// 	} else {
-// 		setState(false);
-// 	}
-// };
